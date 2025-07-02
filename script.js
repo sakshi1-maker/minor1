e4015a738c8514ea7502fa3c5d097d8270799ee7
Sakshi <shrsthsakshi@gmail.com>
2025-07-02 06:59:14 +0545

    initial code

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle dark/light mode (optional)
+const toggleBtn = document.querySelector('.theme-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
